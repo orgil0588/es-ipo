dbb99776437133a557453c20d2a398e376142232
orgil0588 <orgil0588@gmail.com>
2022-05-31 12:42:38 +0800

    spo info spacing fix

--- src/components/SpoInfo.js
+++ src/components/SpoInfo.js
@@ -3,8 +3,8 @@ import SpoInfoItems from "./SpoInfoItems"
 const SpoInfo = () => {
   return (
     <Wrapper>
-      <div className="w-full mx-auto shadow-2xl bg-zinc-800/25 py-4 px-10 md:w-10/12 lg:w-8/12 ">
-        <div className="flex justify-between pb-3 border-b border-primary lg:pb-6">
+      <div className="w-full mx-auto shadow-2xl bg-zinc-800/25 py-4 px-2 md:px-10 md:w-10/12 lg:w-8/12">
+        <div className="grid grid-cols-3 gap-2 pb-3 border-b border-primary lg:pb-6">
           <SpoInfoItems text="Нэгж хувьцаа" desc="108" span="төгрөг" />
           <SpoInfoItems text="Санал болгох" desc="9.5" span="%" />
           <SpoInfoItems text="Татан төвлөрүүлэлт" desc="1.8" span="тэрбум" />
